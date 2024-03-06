e9506f06a0a9246f1782c94d3be7d21ae2cb9e44
vanechek1 <ivankicherov@mail.ru>
2024-03-06 21:10:06 +0500

    commit 3

--- Assets/Scripts/CreateObj.cs
+++ Assets/Scripts/CreateObj.cs
@@ -1,19 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Properties;
 using UnityEngine;
 
 public class CreateObj : MonoBehaviour
 {
     public GameObject[] obj;
-    private GameObject newObj;
-
+    static private GameObject newObj;
+    public GameObject prnt;
     private void Update()
     {
         //GameObject objNext = obj[Random.Range(0, 5)]; 
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            newObj = obj[Random.Range(0, 5)];
-            Instantiate(newObj, transform.position, Quaternion.Euler(0f, 0f, 0f));
+            newObj = newO();
+
+            newObj.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            newObj.GetComponent<Player>().obj = null;
+            newObj.GetComponent<Player>().enabled = false;
         }
+
     }
+
+    private GameObject newO()
+    {
+        newObj = obj[Random.Range(0, 5)];
+        newObj.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        newObj.GetComponent<Player>().enabled = true;
+        newObj.GetComponent<Player>().obj = newObj;
+
+        Instantiate(newObj, transform.position, Quaternion.Euler(0f, 0f, 0f));
+        return newObj;
+    }
+    private void Start()
+    {
+        newO();
+    }
+
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,36 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.Examples;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
     public float speed;
+    public GameObject[] massObj;
     public GameObject obj;
-
+    private GameObject newObj;
     void Update()
     {
-        if(Input.GetKey(KeyCode.D)) // если игрок нажимает клавишу D
+        if (Input.GetKey(KeyCode.D))
         {
-            if(transform.position.x < 4.9)
+            if (transform.position.x < 4.9)
             {
                 obj.transform.Translate(transform.right * speed * Time.deltaTime);
 
             }
-            //rb.velocity = new Vector2(speed, rb.velocity.y);
-            // velocity - скорость
-            // rb.velocity.y - неизменная скорость по игреку
         }
-        if(Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             if (transform.position.x > -4.7)
             {
                 obj.transform.Translate(transform.right * speed * Time.deltaTime * (-1));
             }
-            //rb.velocity = new Vector2(-speed, rb.velocity.y);
-
         }
-
-
+        //if(Input.GetMouseButton(0))
+        //{
+        //    Vector3 mouse = new Vector3(Input.GetAxis("Mouse X") * speed * Time.deltaTime, 0, 0);
+        //    transform.Translate(mouse * speed * 10);
+        //}
+        //if(Input.GetMouseButtonUp(0))
+        //{
+        //    this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        //    this.GetComponent<Player>().enabled = false;
+        //    this.GetComponent<Player>().obj = null;
+        //    SpawnObj();
+        //}
     }
+    //IEnumerator FlareCountdown()
+    //{
+    //    yield return new WaitForSeconds(5f);
+    //}
+    //private void SpawnObj()
+    //{
+    //    newObj = massObj[Random.Range(0, 5)];
+    //    newObj.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+    //    newObj.GetComponent<Player>().enabled = true;
+    //    newObj.GetComponent<Player>().obj = newObj;
+
+    //    Instantiate(newObj, transform.position, Quaternion.Euler(0f, 0f, 0f));
+    //}
+    //private void Start()
+    //{
+    //    SpawnObj();
+    //}
+
 }
