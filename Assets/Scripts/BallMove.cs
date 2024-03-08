a66e70bf4b56a1604dcd4fba442592f12819d9d3
vanechek1 <ivankicherov@mail.ru>
2024-03-08 17:36:36 +0500

    Добавлено слияние и отображение падающего мяча, изменены управление, размеры шаров, цвет и форма "ворот". Исправлены баги

---
+++ Assets/Scripts/BallMove.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour
+{
+    private string inthemask = "y";
+    void Start()
+    {
+        if(transform.position.y < 3.2)
+        {
+            inthemask = "n";
+            GetComponent<Rigidbody2D>().gravityScale = 2;
+        }
+    }
+
+    void Update()
+    {
+        if (inthemask == "y")
+        {
+            GetComponent<Transform>().position = Player.playerxPos;
+        }
+        //if(Input.GetKeyDown(KeyCode.Mouse0))
+        //{
+        //    GetComponent<Rigidbody2D>().gravityScale = 2;
+        //    inthemask = "n";
+        //    Player.spawnedYet = "n";
+        //}
+        if (Input.GetMouseButtonUp(0))
+        {
+            GetComponent<Rigidbody2D>().gravityScale = 2;
+            inthemask = "n";
+            Player.spawnedYet = "n";
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag==gameObject.tag)
+        {
+            Player.spawnPos = transform.position;
+            Player.newBall = "y";
+            Player.whichBall = int.Parse(gameObject.tag);
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,61 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro.Examples;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
     public float speed;
-    public GameObject[] massObj;
+    public GameObject[] balls;
     public GameObject obj;
-    private GameObject newObj;
+    static public string spawnedYet = "n";
+    static public Vector2 playerxPos;
+    static public Vector2 spawnPos;
+    static public string newBall = "n";
+    static public int whichBall = 0;
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
-        {
-            if (transform.position.x < 4.9)
-            {
-                obj.transform.Translate(transform.right * speed * Time.deltaTime);
-
-            }
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            if (transform.position.x > -4.7)
-            {
-                obj.transform.Translate(transform.right * speed * Time.deltaTime * (-1));
-            }
-        }
-        //if(Input.GetMouseButton(0))
+        SpawnBall();
+        replaceFruit();
+        //if(Input.GetKey(KeyCode.D))
         //{
-        //    Vector3 mouse = new Vector3(Input.GetAxis("Mouse X") * speed * Time.deltaTime, 0, 0);
-        //    transform.Translate(mouse * speed * 10);
+        //    if(transform.position.x < 4.9)
+        //    {
+        //        obj.transform.Translate(transform.right * speed * Time.deltaTime);
+        //    }
         //}
-        //if(Input.GetMouseButtonUp(0))
+        //if(Input.GetKey(KeyCode.A))
         //{
-        //    this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-        //    this.GetComponent<Player>().enabled = false;
-        //    this.GetComponent<Player>().obj = null;
-        //    SpawnObj();
+        //    if (transform.position.x > -4.7)
+        //    {
+        //        obj.transform.Translate(transform.right * speed * Time.deltaTime * (-1));
+        //    }
         //}
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mouse = new Vector3(Input.GetAxis("Mouse X") * speed * Time.deltaTime, 0, 0);
+            transform.Translate(mouse * speed * 10);
+        }
+        playerxPos = transform.position;
     }
-    //IEnumerator FlareCountdown()
-    //{
-    //    yield return new WaitForSeconds(5f);
-    //}
-    //private void SpawnObj()
-    //{
-    //    newObj = massObj[Random.Range(0, 5)];
-    //    newObj.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-    //    newObj.GetComponent<Player>().enabled = true;
-    //    newObj.GetComponent<Player>().obj = newObj;
+    void SpawnBall()
+    {
+        if(spawnedYet == "n")
+        {
+            StartCoroutine(SpawnTimer());
+            spawnedYet = "y";
+        }
+    }
+    void replaceFruit()
+    {
+        if(newBall == "y")
+        {
+            newBall = "n";
+            Instantiate(balls[whichBall+1], spawnPos, Quaternion.Euler(0f, 0f, 0f));
+        }
+    }
 
-    //    Instantiate(newObj, transform.position, Quaternion.Euler(0f, 0f, 0f));
-    //}
-    //private void Start()
-    //{
-    //    SpawnObj();
-    //}
+    IEnumerator SpawnTimer()
+    {
+        yield return new WaitForSeconds(.75f);
+        Instantiate(balls[Random.Range(0,5)], transform.position, Quaternion.Euler(0f, 0f, 0f));
+    }
 
 }
