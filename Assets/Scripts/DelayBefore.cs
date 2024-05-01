85911103ca97eccaf4b3d3731dee2c26f8dee896
ant-droid <ant.dobr24@mail.ru>
2024-05-01 16:48:26 +0500

    musicFix

--- Assets/Scripts/DelayBefore.cs
+++ Assets/Scripts/DelayBefore.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,7 @@ public class DelayBefore : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("start game");
     }
 
     // Update is called once per frame
