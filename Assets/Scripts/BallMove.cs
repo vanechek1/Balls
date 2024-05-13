f1860b67a4ad8aabfa0755f28e225e433f7451e4
ant-droid <ant.dobr24@mail.ru>
2024-05-13 13:57:19 +0500

    itchFix

--- Assets/Scripts/BallMove.cs
+++ Assets/Scripts/BallMove.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
